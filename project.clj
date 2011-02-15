e852722bf7d33e6939c5a9c0b4bbd121a11bafac
Robert Campbell <rrc@robert-campbell.com>
2011-02-15 21:14:26 +0100

    installed compojure env

--- project.clj
+++ project.clj
@@ -1,4 +1,7 @@
 (defproject jazeek "1.0.0-SNAPSHOT"
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]])
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [compojure "0.6.0"]]
+  :dev-dependencies [[swank-clojure "1.2.1"]
+                     [lein-ring "0.3.2"]])
