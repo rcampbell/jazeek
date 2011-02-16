8dfef1ec949650779190222ae50dc79c949fa1b8
Georgy Bolyuba <bolyuba@gmail.com>
2011-02-16 23:03:10 +0000

    + added test that should pass :)

--- test/jazeek/test/core.clj
+++ test/jazeek/test/core.clj
@@ -1,6 +1,12 @@
 (ns jazeek.test.core
   (:use [jazeek.core] :reload)
-  (:use [clojure.test]))
+  (:use [clojure.test])
+  (:require [jazeek.db :as db]))
+  
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
+;;(deftest replace-me ;; FIXME: write
+;; (is false "No tests have been written."))
+
+(deftest create-and-read
+         (is (= "Test text" 
+                     ((db/get-block (db/create-block! "Test text")) :text))))
