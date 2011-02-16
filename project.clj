290e7a053f6c00d1c084cbc4cf2b1b5843e271c8
Robert Campbell <rrc@robert-campbell.com>
2011-02-16 15:16:40 +0100

    added db

--- project.clj
+++ project.clj
@@ -2,7 +2,9 @@
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [compojure "0.6.0"]]
+                 [compojure "0.6.0"]
+                 [clojureql "1.0.1"]
+                 [com.h2database/h2 "1.2.147"]]
   :dev-dependencies [[swank-clojure "1.2.1"]
                      [lein-ring "0.3.2"]]
   :ring {:handler jazeek.core/app})
---
+++ src/jazeek/db.clj
@@ -0,0 +1,17 @@
+(ns jazeek.db
+  (:require [clojure.contrib.sql :as sql]
+            [clojureql.core :as cql]))
+
+(def db
+  {:classname   "org.h2.Driver"
+   :subprotocol "h2"
+   :user        "sa"
+   :password    ""
+   :subname     "~/jazeek"})
+
+(cql/open-global db)
+
+(defn init []
+  (sql/with-connection db
+    (sql/create-table :blocks [:text :text])))
+
