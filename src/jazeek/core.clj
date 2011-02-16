15623135cbac1d9ce222dbdd1f2daac7f82ec9e3
Robert Campbell <rrc@robert-campbell.com>
2011-02-16 21:02:23 +0100

    got the C and R in CRUD working with a basic text block

--- src/jazeek/core.clj
+++ src/jazeek/core.clj
@@ -1,11 +1,19 @@
 (ns jazeek.core
   (:use compojure.core)
   (:require [compojure.route :as route]
-            [compojure.handler :as handler]))
+            [compojure.handler :as handler]
+            [compojure.response :as response]
+            [jazeek.db :as db]))
+
+(defn create-block! [text]
+  (let [id (db/create-block! text)]
+    {:status 301
+     :headers {"Location" (str "/blocks/" id)}}))
 
 (defroutes main-routes
-  (GET "/" [] "It works")
-  (route/resources "/")
+  (GET  "/"           []                 (response/resource "index.html"))
+  (GET  "/blocks/:id" [id]               (db/get-block id))
+  (POST "/blocks"     [& {:keys [text]}] (create-block! text))
   (route/not-found "Page not found"))
 
 (def app
--- src/jazeek/db.clj
+++ src/jazeek/db.clj
@@ -2,6 +2,10 @@
   (:require [clojure.contrib.sql :as sql]
             [clojureql.core :as cql]))
 
+;; TODO
+;; - separate the "migration" logic (c.c.sql) from the "runtime" (cql)
+;; - catch PK constraint violations, looping with new id
+
 (def db
   {:classname   "org.h2.Driver"
    :subprotocol "h2"
@@ -13,5 +17,26 @@
 
 (defn init! []
   (sql/with-connection db
-    (sql/create-table :blocks [:text :text])))
+    (sql/create-table :blocks
+                      [:id "varchar(6)" "PRIMARY KEY"]
+                      [:text :text])))
+
+(defn- create-id! []
+  (let [alphanumeric (map char (concat (range 48 58)    ; 0-9
+                                       (range 65 91)    ; A-Z
+                                       (range 97 123))) ; a-z
+        confusing #{\0 \O \o \1 \L \l \5 \S \s}
+        pool (remove confusing alphanumeric)]
+    (comment "# permutations" (Math/pow (count pool) 6))
+    (->> pool shuffle (take 6) (apply str))))
+
+(let [blocks (cql/table :blocks)]
+  (defn get-block [id]
+    (cql/project (cql/select blocks (cql/where (= :id id))) [:text]))
+  (defn create-block! [text]
+    (let [id (create-id!)]
+      (cql/conj! blocks {:id id :text text})
+      id)))
+
+ 
 
