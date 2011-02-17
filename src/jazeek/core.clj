53da2ae148f8ed423368cbff177411dd240c1694
Robert Campbell <rrc@robert-campbell.com>
2011-02-17 17:29:01 +0100

    added update and delete

--- src/jazeek/core.clj
+++ src/jazeek/core.clj
@@ -5,15 +5,44 @@
             [compojure.response :as response]
             [jazeek.db :as db]))
 
+(letfn [(redirect [status]
+                  (fn [uri] {:status status
+                            :headers {"Location" uri}}))]
+  (def ^{:doc "moved permanently" :private true}
+    moved-to  (redirect 301)))
+
 (defn create-block! [text]
   (let [id (db/create-block! text)]
-    {:status 301
-     :headers {"Location" (str "/blocks/" id)}}))
+    (moved-to (str "/blocks/" id))))
+
+
+
+(defn get-block [id]
+  (let [clob (:text (first @(db/get-block id)))
+        text (.getSubString clob 1 (.length clob))] ; TODO: replace w/reader
+    text)) 
+
+(defn update-block! [params]
+  (println ">>> " params)
+  ;[{id :id, :as block}]
+;  (db/update-block! block)
+ ; (get-block id)
+  )
+
+(defn delete-block! [id]
+  (db/delete-block! id)
+  (moved-to "/"))
+
+(defn list-blocks []
+  "TODO")
 
 (defroutes main-routes
-  (GET  "/"           []                 (response/resource "index.html"))
-  (GET  "/blocks/:id" [id]               (db/get-block id))
-  (POST "/blocks"     [& {:keys [text]}] (create-block! text))
+  (GET    "/"           []                 (response/resource "index.html"))
+  (GET    "/blocks/"    []                 (list-blocks))
+  (POST   "/blocks/"    [& {:keys [text]}] (create-block! text))
+  (GET    "/blocks/:id" [id]               (get-block id))
+  (PUT    "/blocks/:id" [& params]         (update-block! params))
+  (DELETE "/blocks/:id" [id]               (db/delete-block! id))
   (route/not-found "Page not found"))
 
 (def app
--- src/jazeek/db.clj
+++ src/jazeek/db.clj
@@ -2,8 +2,8 @@
   (:require [clojure.contrib.sql :as sql]
             [clojureql.core :as cql]))
 
-;; TODO
-;; - separate the "migration" logic (c.c.sql) from the "runtime" (cql)
+;; TODO:
+;; - separate the DDL (c.c.sql) from the DML (cql)
 ;; - catch PK constraint violations, looping with new id
 
 (def db
@@ -15,28 +15,30 @@
 
 (cql/open-global db)
 
-(defn init! []
+(defn init!
+  "creates the schema"
+  []
   (sql/with-connection db
     (sql/create-table :blocks
                       [:id "varchar(6)" "PRIMARY KEY"]
                       [:text :text])))
 
-(defn- create-id! []
-  (let [alphanumeric (map char (concat (range 48 58)    ; 0-9
-                                       (range 65 91)    ; A-Z
-                                       (range 97 123))) ; a-z
-        confusing #{\0 \O \o \1 \L \l \5 \S \s}
-        pool (remove confusing alphanumeric)]
-    (comment "# permutations" (Math/pow (count pool) 6))
-    (->> pool shuffle (take 6) (apply str))))
-
 (let [blocks (cql/table :blocks)]
+  (letfn [(create-id!
+           []
+           (let [alphanumeric (map char (concat (range 48 58)    ; 0-9
+                                                (range 65 91)    ; A-Z
+                                                (range 97 123))) ; a-z
+                 confusing #{\0 \O \o \1 \L \l \5 \S \s}
+                 pool (remove confusing alphanumeric)]
+             (->> pool shuffle (take 6) (apply str))))]
+    (defn create-block! [text]
+      (let [id (create-id!)]
+        (cql/conj! blocks {:id id :text text})
+        id)))
   (defn get-block [id]
     (cql/project (cql/select blocks (cql/where (= :id id))) [:text]))
-  (defn create-block! [text]
-    (let [id (create-id!)]
-      (cql/conj! blocks {:id id :text text})
-      id)))
-
- 
-
+  (defn update-block! [{:keys [id text]}]
+    (cql/update-in! blocks (cql/where (= :id id)) {:text text}))
+  (defn delete-block! [id]
+    (cql/disj! blocks (cql/where (= :id id)))))
