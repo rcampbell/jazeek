2b5f7ec34e71a12b24f3aab6f804b7c467c545f2
Georgy Bolyuba <bolyuba@gmail.com>
2011-03-18 21:04:35 +0000

    + Added slf4j-simple dependiency to see exceptions from jetty. Way easier to debug stuff this way

--- project.clj
+++ project.clj
@@ -7,7 +7,8 @@
                  [clojureql "1.0.1"]
                  [com.h2database/h2 "1.2.147"]
                  [sandbar/sandbar "0.3.0"]
-                 [http.async.client "0.2.2"]]
+                 [http.async.client "0.2.2"]
+                 [org.slf4j/slf4j-simple "1.6.1"]]
   :dev-dependencies [[swank-clojure "1.2.1"]
                      [lein-ring "0.3.2"]]
   :ring {:handler jazeek.core/app})
